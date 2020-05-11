c7dde2b05f8134d76abfedc1f698193aed880b2c
RT0620 <tyagirishabh19990620@gmail.com>
2020-05-11 18:43:09 +0530

    Updates

---
+++ precache-manifest.988f0fca94d775e34e99f21689691008.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6db1e86f1cf479038f76e124c07f8d4",
+    "url": "/CollegeProject/index.html"
+  },
+  {
+    "revision": "bb56c984d1bfccc24809",
+    "url": "/CollegeProject/static/css/main.d7eae7ac.chunk.css"
+  },
+  {
+    "revision": "2d3964304f559c980363",
+    "url": "/CollegeProject/static/js/2.24735a5a.chunk.js"
+  },
+  {
+    "revision": "b242d1ab830c47401dfff3b146ff3f46",
+    "url": "/CollegeProject/static/js/2.24735a5a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bb56c984d1bfccc24809",
+    "url": "/CollegeProject/static/js/main.85d85065.chunk.js"
+  },
+  {
+    "revision": "3baefd672299483b8275",
+    "url": "/CollegeProject/static/js/runtime-main.0eac450f.js"
+  },
+  {
+    "revision": "3823c1cc94824e18c55912442a4d794b",
+    "url": "/CollegeProject/static/media/back.3823c1cc.svg"
+  },
+  {
+    "revision": "ed28accb7dd58a0469bff983503ff07e",
+    "url": "/CollegeProject/static/media/crwn.ed28accb.svg"
+  }
+]);
